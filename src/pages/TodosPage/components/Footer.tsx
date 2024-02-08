6594f11d85c72f388abfcbfecea9cc68d1f14f68
Roman Karpii <romankarpiy8@gmail.com>
2024-02-09 00:34:54 +0100

    modified:   src/pages/TodosPage/components/Footer.tsx

--- src/pages/TodosPage/components/Footer.tsx
+++ src/pages/TodosPage/components/Footer.tsx
@@ -2,6 +2,6 @@ import FooterType from '../types/FooterType'
 
 export default function Footer({ countOfActiveTodos, countOfCompletedTodos }: FooterType) {
     return (
-        <footer className="app-footer"><span className='count'>{countOfActiveTodos}</span> more to do, <span className='count'>{countOfCompletedTodos}</span> done</footer>
+        <footer className="app-footer p-4"><span className='count  text-warning'>{countOfActiveTodos}</span> more to do, <span className='count  text-warning'>{countOfCompletedTodos}</span> done</footer>
     )
 }
