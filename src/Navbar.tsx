a58d73f0c086e9477efd5c186cedc19d9c3f4c0f
Roman Karpii <romankarpiy8@gmail.com>
2024-02-10 00:02:42 +0100

    modified:   src/Navbar.tsx

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -6,7 +6,7 @@ interface NavbarProps {
 export default function Navbar({ setPage, currentPage }: NavbarProps) {
     return (
         <div>
-            <nav className="navbar bg-body-tertiary">
+            <nav className="navbar bg-body-tertiary border-bottom">
                 <form className="container-fluid justify-content-center">
                     <button className={`btn btn-outline-warning me-2 ${currentPage === 'notes' ? 'bg-warning text-black' : ''}`} type="button" onClick={() => setPage('notes')}>Notes</button>
                     <button className={`btn btn-outline-warning me-2 ${currentPage === 'todos' ? 'bg-warning text-black' : ''}`} type="button" onClick={() => setPage('todos')}>Todos</button>
