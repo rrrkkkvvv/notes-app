7c942dc23d39921c6c6b8fd585531230f0703d91
Roman Karpii <romankarpiy8@gmail.com>
2024-02-14 22:02:02 +0100

    modified:   src/pages/NotesPage/components/fullNote/FullNote.tsx

--- src/pages/NotesPage/components/fullNote/FullNote.tsx
+++ src/pages/NotesPage/components/fullNote/FullNote.tsx
@@ -33,7 +33,7 @@ const FullNote: React.FC<FullNotePorps> = (props) => {
                         <span className='close-modal-x' onClick={props.toogleShowFullNote}><IoReturnDownBackOutline ></IoReturnDownBackOutline ></span>
                         <span className='fullNote-date-block'>{props.currentFullNote.date}</span>
                         <span className='fullNote-text-length-block'>{props.currentFullNote.text.length} symblos</span>
-                        <input type='text' className='input-fullNote' placeholder='Title...' value={props.currentFullNote.title} onChange={
+                        <input type='text' className='input-fullNote' placeholder='Title...' value={props.currentFullNote.title.trim()} onChange={
                             (e) => {
                                 onInputTitle(e);
                             }
@@ -78,7 +78,7 @@ const FullNote: React.FC<FullNotePorps> = (props) => {
                     }}><IoReturnDownBackOutline ></IoReturnDownBackOutline ></span>
                     <span className='fullNote-date-block'></span>
                     <span className='fullNote-text-length-block'>{mainText.length} symblos</span>
-                    <input type='text' className='input-fullNote' placeholder='Title...' value={titleText} onChange={(e) => {
+                    <input type='text' className='input-fullNote' placeholder='Title...' value={titleText.trim()} onChange={(e) => {
                         setTitleText(e.target.value);
 
                     }} />
